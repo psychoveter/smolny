8249cab725cd789e468bb5c5d8adff42c5f653e7
Osinidi <anastasiaosina@yandex.ru>
2015-04-11 04:23:50 +0300

    Create CreateRandomMatrix.java

---
+++ A_Osina/Matrices/CreateRandomMatrix.java
@@ -0,0 +1,30 @@
+public class CreateRandomMatrix {
+	
+	public static void main(String[] args) {
+
+		int[][] A = new int[5][5];
+		for(int i = 0; i < A.length; i++) {
+			for(int j = 0; j < A[i].length; j++) {
+				A[i][j] = (int) (Math.random()*21)-10;
+			}
+		}
+		for(int i = 0; i < A.length; i++) {
+			for(int j = 0; j < A[i].length; j++) {
+			}
+		}
+		String result = arrToString(A);
+		System.out.print(result);
+	}
+
+	public static String arrToString(int[][] arr) {
+	String str = "[";
+		for (int i = 0; i < arr.length; i++){
+			for(int j = 0; j < arr[i].length; j++)
+				
+				str += arr[i][j] + ",";
+				str += "]" + "\n" + "[";
+				continue;
+			}
+		return str;
+	}
+}
